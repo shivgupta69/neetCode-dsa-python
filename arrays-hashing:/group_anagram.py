5b1a01a7fb918ddf62744ff0001b1916e08e9cbb
Shiv Gupta <itz47shiv@gmail.com>
2026-03-25 21:21:01 +0530

    Update group_anagram.py

--- arrays-hashing:/group_anagram.py
+++ arrays-hashing:/group_anagram.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list) # first created a dict which contains all values in list
 
-        for s in strs: # for loop iterate into strs
+        for s in strs: # for loop iterate_into strs
             count = [0] * 26
             for c in s: # iterate inside s
                 count[ord(c) - ord('a')] += 1 # aski no of alphabet's - aski no of a 
