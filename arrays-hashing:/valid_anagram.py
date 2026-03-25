182b4a38111c83572f5c485bdfcd6976e5e96ec2
Shiv Gupta <itz47shiv@gmail.com>
2026-03-25 21:22:47 +0530

    Improve comments in isAnagram method
    Refactor comments for clarity and conciseness.

--- arrays-hashing:/valid_anagram.py
+++ arrays-hashing:/valid_anagram.py
@@ -1,13 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        ## at first i will verify the length of s = t.
-        ## if not same returns false.
+     ## first match the length of both arrays
         if len(s) != len(t):
             return False
-
-        ## is the length is same then
-        ## I will sort them and match with each other 
-        
-        ## if it matches s = t then return true.
+    ## if the lenth matches sort them and match .. 
         return  sorted(s) == sorted(t)
