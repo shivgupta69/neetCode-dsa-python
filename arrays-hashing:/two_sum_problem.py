54197207ac68becbe19edeff36c452d0ff471030
Shiv Gupta <itz47shiv@gmail.com>
2026-04-15 21:28:38 +0530

    Update two_sum_problem.py

--- arrays-hashing:/two_sum_problem.py
+++ arrays-hashing:/two_sum_problem.py
@@ -6,7 +6,7 @@ class Solution:
         for i, n in enumerate(nums): ## using for loop to get the
         ## index no and value.
             diff = target - n  ## the provided target - value
-            ## store the diff in hashmap and iterate another value
+        
             if diff in prevMap:
                 return [prevMap[diff], i] 
             ## if the difference is already persent in pervmap
